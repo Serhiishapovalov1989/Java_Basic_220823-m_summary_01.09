e1f15dda4629bf8d5e9fa83df3441203c2574f63
Serhiishapovalov1989 <desiderades@gmail.com>
2023-09-01 13:00:51 +0300

    Init Commit

---
+++ src/main/java/de/telran/HomeWork_n1_Lesson2/ParseNumber.java
@@ -0,0 +1,10 @@
+package de.telran.HomeWork_n1_Lesson2;
+
+public class ParseNumber {
+    public static void main(String[] args) {
+        int myNum = 345;
+        System.out.println(myNum % 10);
+        System.out.println(myNum / 10 % 10);
+        System.out.println(myNum / 100);
+    }
+}
