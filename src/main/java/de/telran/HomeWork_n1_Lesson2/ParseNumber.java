01895abf8b08adca1c3515b4a26768252fdc9347
Serhiishapovalov1989 <130730415+Serhiishapovalov1989@users.noreply.github.com>
2023-09-07 20:31:34 +0300

    Update ParseNumber.java

--- src/main/java/de/telran/HomeWork_n1_Lesson2/ParseNumber.java
+++ src/main/java/de/telran/HomeWork_n1_Lesson2/ParseNumber.java
@@ -1,10 +1,39 @@
-package de.telran.HomeWork_n1_Lesson2;
+package homework.Number_1;
 
-public class ParseNumber {
+public class Main {
     public static void main(String[] args) {
-        int myNum = 345;
-        System.out.println(myNum % 10);
-        System.out.println(myNum / 10 % 10);
-        System.out.println(myNum / 100);
+        char ch = 'G';
+        int in = 89;
+        byte by = 4;
+        short sh = 56;
+        float fl = 4.7333436F;
+        double dub = 4.355453532;
+        long lo = 12121;
+
+        System.out.println("Character: " + ch);
+        System.out.println();
+        System.out.println("Integer: " + in);
+        System.out.println();
+        System.out.println("Byte: " + by);
+        System.out.println();
+        System.out.println("Short: " + sh);
+        System.out.println();
+        System.out.println("Float: " + fl);
+        System.out.println();
+        System.out.println("Double: " + dub);
+        System.out.println();
+        System.out.println("Long: " + lo);
+        System.out.println();
+
+
+        //________________________________________________________________________________//
+
+
+        int intN = 345;
+        int firstNumber = intN/100;
+        int secondNumber = intN%100/10;
+        int thirdNumber = intN%10;
+
+        System.out.println("Число: " + 345 + " -> " + firstNumber + " , " + secondNumber + " , " + thirdNumber + " . ");
     }
 }
